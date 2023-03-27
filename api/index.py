9a85e0bc65d36924a188bdde319208042a678dea
mace.zhang <1226710837@qq.com>
2023-03-27 11:41:55 +0800

    fix index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,11 @@
 from sanic import Sanic, Request
 from sanic.response import text
 app = Sanic("index")
- 
+
  
 @app.route('/')
 async def index(request:Request):
-    print("foejfoiwejfo")
     return text("hello sanic + vercel")
+
+if __name__ == "__main__":
+    app.run(single_process=True)
